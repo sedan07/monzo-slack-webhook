6fb9f56467f56a53e4cebf76be1e8bb3bfad544a
Seb Dangerfield <1449113+sedan07@users.noreply.github.com>
2017-08-28 21:03:53 +0100

    Improving styling of spec/ inline with eslint

--- spec/format-message-spec.js
+++ spec/format-message-spec.js
@@ -1,76 +1,71 @@
 const formatMessage = require('../lib/format-message');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
-describe('formatMessage()', function() {
+describe('formatMessage()', () => {
+  describe('amount', () => {
+    let event;
 
-  describe('amount', function() {
-    var event;
-
-    beforeEach(function() {
-      const rawEvent = fs.readFileSync(path.join(__dirname, '../example_data/transaction1.json'))
+    beforeEach(() => {
+      const rawEvent = fs.readFileSync(path.join(__dirname, '../example_data/transaction1.json'));
       event = JSON.parse(rawEvent).data;
     });
 
-    it('Correctly inserts the decimal point', function() {
-      var message = formatMessage(event)
+    it('Correctly inserts the decimal point', () => {
+      const message = formatMessage(event);
 
-      expect(message).toMatch(/3\.50/)
+      expect(message).toMatch(/3\.50/);
     });
 
-    it('Correctly converts all values into absolute ones', function() {
-      var message = formatMessage(event)
+    it('Correctly converts all values into absolute ones', () => {
+      const message = formatMessage(event);
 
-      expect(message).toMatch(/[^\-\+]3\.50/)
+      expect(message).toMatch(/[^\-\+]3\.50/);
     });
-
   });
 
-  describe('direction', function() {
-    var event;
+  describe('direction', () => {
+    let event;
 
-    it('Correctly detects debits', function() {
+    it('Correctly detects debits', () => {
       event = {
-        "account_id": "acc_00008gju41AHyfLUzBUk8A",
-        "amount": -350,
-        "created": "2015-09-04T14:28:40Z",
-        "currency": "GBP",
-        "description": "Ozone Coffee Roasters"
-      }
-      var message = formatMessage(event)
+        account_id: 'acc_00008gju41AHyfLUzBUk8A',
+        amount: -350,
+        created: '2015-09-04T14:28:40Z',
+        currency: 'GBP',
+        description: 'Ozone Coffee Roasters',
+      };
+      const message = formatMessage(event);
 
-      expect(message).toMatch(/charged/)
+      expect(message).toMatch(/charged/);
     });
 
-    it('Correct detects credits', function() {
+    it('Correct detects credits', () => {
       event = {
-        "account_id": "acc_00008gju41AHyfLUzBUk8A",
-        "amount": 350,
-        "created": "2015-09-04T14:28:40Z",
-        "currency": "GBP",
-        "description": "Ozone Coffee Roasters"
-      }
-      var message = formatMessage(event)
+        account_id: 'acc_00008gju41AHyfLUzBUk8A',
+        amount: 350,
+        created: '2015-09-04T14:28:40Z',
+        currency: 'GBP',
+        description: 'Ozone Coffee Roasters',
+      };
+      const message = formatMessage(event);
 
-      expect(message).toMatch(/credited/)
+      expect(message).toMatch(/credited/);
     });
-
   });
 
-  describe('who', function() {
-    var event;
+  describe('who', () => {
+    let event;
 
-    beforeEach(function() {
-      const rawEvent = fs.readFileSync(path.join(__dirname, '../example_data/transaction1.json'))
+    beforeEach(() => {
+      const rawEvent = fs.readFileSync(path.join(__dirname, '../example_data/transaction1.json'));
       event = JSON.parse(rawEvent).data;
     });
 
-    it('Correctly inserts the account', function() {
-      var message = formatMessage(event)
+    it('Correctly inserts the account', () => {
+      const message = formatMessage(event);
 
-      expect(message).toMatch(/Ozone Coffee Roasters/)
+      expect(message).toMatch(/Ozone Coffee Roasters/);
     });
-
   });
-
 });
--- spec/slack-webhook-spec.js
+++ spec/slack-webhook-spec.js
@@ -1,53 +1,51 @@
-const slackWebhook = require('../lib/slack-webhook')
-const nock = require('nock')
+const slackWebhook = require('../lib/slack-webhook');
+const nock = require('nock');
 
-describe('slackWebhook', function() {
+describe('slackWebhook', () => {
+  describe('sendMessage', () => {
+    it('Should make a HTTP request to hooks.slack.com', (done) => {
+      const scope = nock('https://hooks.slack.com')
+        .post('/services/THISISDUMMY/URL')
+        .reply(200, 'ok');
 
-  describe('sendMessage', function() {
-    it('Should make a HTTP request to hooks.slack.com', function(done) {
-      var scope = nock('https://hooks.slack.com')
-          .post('/services/THISISDUMMY/URL')
-          .reply(200, 'ok');
-
-      var slack = new slackWebhook({
-        url: 'https://hooks.slack.com/services/THISISDUMMY/URL'
+      const slack = new slackWebhook({
+        url: 'https://hooks.slack.com/services/THISISDUMMY/URL',
       });
 
-      slack.sendMessage('test').then(function() {
-        expect(scope.isDone()).toBe(true)
-        done()
-      })
-    });
-
-    it('Should reject the promise if slack returns an error', function(done) {
-      var scope = nock('https://hooks.slack.com')
-          .post('/services/THISISDUMMY/URL')
-          .reply(400, 'ok');
-
-      var slack = new slackWebhook({
-        url: 'https://hooks.slack.com/services/THISISDUMMY/URL'
-      });
-
-      slack.sendMessage('test').then(function() {
-        expect('Promise not to resolve').toBe(false)
-        done()
-      }).catch(function() {
-        expect(scope.isDone()).toBe(true)
-        done()
+      slack.sendMessage('test').then(() => {
+        expect(scope.isDone()).toBe(true);
+        done();
       });
     });
 
-    it('Should throw an error if URL is not set', function(done) {
-      var slack = new slackWebhook();
+    it('Should reject the promise if slack returns an error', (done) => {
+      const scope = nock('https://hooks.slack.com')
+        .post('/services/THISISDUMMY/URL')
+        .reply(400, 'ok');
 
-      slack.sendMessage('test').then(function() {
-        expect('Promise not to resolve').toBe(false)
-        done()
-      }).catch(function(e) {
-        expect(e).toMatch(/slack webhook url/)
-        done()
+      const slack = new slackWebhook({
+        url: 'https://hooks.slack.com/services/THISISDUMMY/URL',
+      });
+
+      slack.sendMessage('test').then(() => {
+        expect('Promise not to resolve').toBe(false);
+        done();
+      }).catch(() => {
+        expect(scope.isDone()).toBe(true);
+        done();
+      });
+    });
+
+    it('Should throw an error if URL is not set', (done) => {
+      const slack = new slackWebhook();
+
+      slack.sendMessage('test').then(() => {
+        expect('Promise not to resolve').toBe(false);
+        done();
+      }).catch((e) => {
+        expect(e).toMatch(/slack webhook url/);
+        done();
       });
     });
   });
-
 });
