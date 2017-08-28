33f4d9125c16d9f5ad14b29468ddd77125aa4804
Seb Dangerfield <1449113+sedan07@users.noreply.github.com>
2017-08-28 12:26:15 +0100

    Minor cleanup of slack webhook code and specs sendMessage()

--- spec/slack-webhook-spec.js
+++ spec/slack-webhook-spec.js
@@ -4,20 +4,50 @@ const nock = require('nock')
 describe('slackWebhook', function() {
 
   describe('sendMessage', function() {
-    it('Should make a HTTP request to hooks.slack.com', function() {
+    it('Should make a HTTP request to hooks.slack.com', function(done) {
       var scope = nock('https://hooks.slack.com')
           .post('/services/THISISDUMMY/URL')
-          .reply(200, '');
+          .reply(200, 'ok');
 
       var slack = new slackWebhook({
         url: 'https://hooks.slack.com/services/THISISDUMMY/URL'
       });
-      slack.sendMessage('test')
 
-      expect(scope.isDone()).toBe(true)
-    
+      slack.sendMessage('test').then(function() {
+        expect(scope.isDone()).toBe(true)
+        done()
+      })
     });
 
+    it('Should reject the promise if slack returns an error', function(done) {
+      var scope = nock('https://hooks.slack.com')
+          .post('/services/THISISDUMMY/URL')
+          .reply(400, 'ok');
+
+      var slack = new slackWebhook({
+        url: 'https://hooks.slack.com/services/THISISDUMMY/URL'
+      });
+
+      slack.sendMessage('test').then(function() {
+        expect('Promise not to resolve').toBe(false)
+        done()
+      }).catch(function() {
+        expect(scope.isDone()).toBe(true)
+        done()
+      });
+    });
+
+    it('Should throw an error if URL is not set', function(done) {
+      var slack = new slackWebhook();
+
+      slack.sendMessage('test').then(function() {
+        expect('Promise not to resolve').toBe(false)
+        done()
+      }).catch(function(e) {
+        expect(e).toMatch(/slack webhook url/)
+        done()
+      });
+    });
   });
 
 });
